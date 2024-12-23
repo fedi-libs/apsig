660d60e9425d070d4e11068ebc130bee1ff493ae
AmaseCocoa <amase.cocoa@gmail.com>
2024-12-23 17:00:53 +0900

    fix: Implementing the correct LD Signature

--- src/apsig/__init__.py
+++ src/apsig/__init__.py
@@ -2,6 +2,6 @@ from .object_integrity_proofs.sign import OIPSigner
 from .object_integrity_proofs.verify import OIPVerifier
 from .draft.sign import draftSigner
 from .draft.verify import draftVerifier
-from .ld.ldsig import JSONLDSigner
+from .ld_signature import LDSignature
 
-__all__ = ["OIPSigner", "OIPVerifier" , "draftSigner", "draftVerifier", "JSONLDSigner"]
+__all__ = ["OIPSigner", "OIPVerifier" , "draftSigner", "draftVerifier", "LDSignature"]
--- tests/test_ld.py
+++ tests/test_ld.py
@@ -1,57 +1,53 @@
 import unittest
-from apsig import JSONLDSigner
+
+from apsig import LDSignature
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from apsig.exceptions import MissingSignature
 
 class TestJsonLdSigner(unittest.TestCase):
-
     def setUp(self):
-        self.signer = JSONLDSigner()
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.public_key = self.private_key.public_key()
+        self.ld = LDSignature()
         self.data = {
             "@context": [
                 "https://www.w3.org/ns/activitystreams",
                 "https://w3id.org/security/v1",
-                {
-                    "Key": "sec:Key",
-                    "manuallyApprovesFollowers": "as:manuallyApprovesFollowers",
-                    "sensitive": "as:sensitive",
-                    "Hashtag": "as:Hashtag",
-                    "quoteUrl": "as:quoteUrl",
-                    "toot": "http://joinmastodon.org/ns#",
-                    "Emoji": "toot:Emoji",
-                    "featured": "toot:featured",
-                    "discoverable": "toot:discoverable",
-                    "schema": "http://schema.org#",
-                    "PropertyValue": "schema:PropertyValue",
-                    "value": "schema:value",
-                    "misskey": "https://misskey-hub.net/ns#",
-                    "_misskey_content": "misskey:_misskey_content",
-                    "_misskey_quote": "misskey:_misskey_quote",
-                    "_misskey_reaction": "misskey:_misskey_reaction",
-                    "_misskey_votes": "misskey:_misskey_votes",
-                    "_misskey_summary": "misskey:_misskey_summary",
-                    "isCat": "misskey:isCat",
-                    "vcard": "http://www.w3.org/2006/vcard/ns#"
-                }
             ],
             "type": "Note",
             "content": "Hello, world!"
         }
-        self.headers = {
-            'host': 'example.com'
-        }
+        self.signed_data = self.ld.sign(self.data, "https://example.com/users/johndoe#main-key", private_key=self.private_key)
 
     def test_sign_and_verify(self):
-        signature_header, normalized_data = self.signer.sign(self.data, self.headers, "POST", "https://example.com/inbox", "https://example.com/users/johndoe#main-key")
-
-        is_valid = self.signer.verify(signature_header, normalized_data, self.headers)
+        is_valid = self.ld.verify(self.signed_data, self.public_key)
         self.assertTrue(is_valid)
 
-    def test_verify_invalid_signature(self):
-        signature_header, normalized_data = self.signer.sign(self.data, self.headers, "POST", "https://example.com/inbox", "https://example.com/users/johndoe#main-key")
-
-        signature_header['signature'] = 'invalid_signature'
-
-        is_valid = self.signer.verify(signature_header, normalized_data, self.headers)
+    def test_verify_invalid_signature_value(self):
+        signed_data = self.ld.sign(self.data, "https://example.com/users/johndoe#main-key", private_key=self.private_key)
+        signed_data["signature"]["signatureValue"] = "invalid_signature"
+        is_valid = self.ld.verify(signed_data, self.public_key)
         self.assertFalse(is_valid)
+        
+    def test_verify_missing_signature(self):
+        try:
+            self.ld.verify(self.data, self.public_key)
+            is_fail = False
+        except MissingSignature:
+            is_fail = True
+        self.assertTrue(is_fail)
+        
+    def test_verify_invalid_signature(self):
+        signed_data = self.ld.sign(self.data, "https://example.com/users/johndoe#main-key", private_key=self.private_key)
+        signed_data["signature"]["type"] = "RsaSignatureHoge"
+        is_fail = self.ld.verify(signed_data, self.public_key)
+        self.assertTrue(is_fail)
 
 if __name__ == '__main__':
     unittest.main()
